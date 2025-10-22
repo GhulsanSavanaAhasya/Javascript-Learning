a6d79d4506ea278590deec722636960e3d3cf072
unknown <ghulsan.savanaa@gmail.com>
2025-10-22 23:17:51 +0700

    increment decrement

--- while/script.js
+++ while/script.js
@@ -1,5 +1,11 @@
-let number = 1;
-while(number <= 10){
-    console.log(`Hello World! ${number}`);
-    number++;
+// let number = 1;
+// while(number <= 10){
+//     console.log(`Hello World! ${number}`);
+//     number++;
+// }
+let i = 10;
+
+while( i >= 1){
+    console.log("Hallo!" + i);
+    i--;
 }
