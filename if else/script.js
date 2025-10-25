80267aeccbe0703bc3adcf3dc32750b9efab9b37
unknown <ghulsan.savanaa@gmail.com>
2025-10-25 10:38:33 +0700

    if else

---
+++ if else/script.js
@@ -0,0 +1,13 @@
+// let i = 1;
+// if(i < 0){
+//     console.log("It's true");
+// }else{
+//     console.log("It's false");
+// }
+
+let input = prompt(`Input number: `);
+if(input % 2 == 0){
+    alert(`${input} is even`);
+}else{
+    alert(`${input} is odd`);
+}
