d9a4cfab8e383e6231c8192705ef6bd3b19dc677
unknown <ghulsan.savanaa@gmail.com>
2025-10-25 11:44:52 +0700

    switch case

---
+++ switch case/script.js
@@ -0,0 +1,27 @@
+let day = prompt("Input day: ");
+let say;
+switch(day){
+    case "Monday":
+        alert("It's monday")
+        break;
+    case "Tuesday":
+        alert("It's tuesday");
+        break;
+    case "Wednesday":
+        alert("It's wednesday");
+        break;
+    case "Thursday":
+        alert("It's thursday");
+        break;
+    case "Friyay":
+        alert("It's friyay");
+        break;
+    case "Saturday":
+    case "Sunday":
+        alert("It's holiday");
+        break;
+    default:
+        alert("Bored day");
+}
+
+alert("Thankyoyu");
