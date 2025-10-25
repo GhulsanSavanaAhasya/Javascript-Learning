4c5b4c8d6c005b18ca7a1fd48dd7f93cd942ba97
unknown <ghulsan.savanaa@gmail.com>
2025-10-25 11:10:01 +0700

    add do while looping

--- if else if/script.js
+++ if else if/script.js
@@ -1,9 +1,14 @@
-let input = prompt("Input number: ");
+alert("Welcome!")
 
-if(input % 2 == 0){
-    alert(`${input} is even`);
-}else if(input % 2 == 1){
-    alert(`${input} is odd`);
-}else {
-    alert(`${input} is not a number`);
-}
+do{
+    let input = prompt("Input number: ");
+    if(input % 2 == 0){
+        alert(`${input} is even`);
+    }else if(input % 2 == 1){
+        alert(`${input} is odd`);
+    }else {
+        alert(`${input} is not a number`);
+    }
+}while(confirm("Want to try again?"));
+
+alert("Thank you!");
