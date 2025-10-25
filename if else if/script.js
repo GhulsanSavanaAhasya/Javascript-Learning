c3baf3507fa7f6b3da36bb6c5bb912260fff7a38
unknown <ghulsan.savanaa@gmail.com>
2025-10-25 11:04:34 +0700

    if else if

---
+++ if else if/script.js
@@ -0,0 +1,9 @@
+let input = prompt("Input number: ");
+
+if(input % 2 == 0){
+    alert(`${input} is even`);
+}else if(input % 2 == 1){
+    alert(`${input} is odd`);
+}else {
+    alert(`${input} is not a number`);
+}
