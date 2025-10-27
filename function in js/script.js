88189549ae1a4ba71bd36c7f8301ed9f9c166fa3
unknown <ghulsan.savanaa@gmail.com>
2025-10-27 23:06:56 +0700

    learn about function

---
+++ function in js/script.js
@@ -0,0 +1,34 @@
+//function declaration
+// function myFunction(){
+//     console.log("Hello World!");
+// }
+
+// myFunction();
+
+//function expression
+// let myFunction = function(){
+//     console.log("Ghulsan Savana Ahasya");
+// }
+// myFunction();
+
+// let plus = function(a, b){
+//     return a + b;
+// }
+
+// console.log(plus(5, 10));
+
+// function times(a, b){
+//     return a * b;
+// }
+// console.log(times(10, 10));
+
+//arrow function
+// let myFunction = (a, b) =>{
+//     return a / b;
+// }
+// console.log(myFunction(10, 10));
+
+let hello = () =>{
+    return alert("Hello World!");
+}
+hello();
